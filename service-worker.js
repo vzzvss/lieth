03f4dd1fd13ab369d05e2f4535f365c4093e0ddb
vzzvss <code5.sa@outlook.sa>
2025-12-10 15:30:55 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'laith-v2';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
